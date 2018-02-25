8fba0bfcfb73d6315cf39678aa728fdb6b555396
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2018-02-25 16:20:54 -0300

    Menu inicial

--- Assets/scripts/UI/Buttons.cs
+++ Assets/scripts/UI/Buttons.cs
@@ -2,13 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Buttons : MonoBehaviour {
     public AudioClip ambient;
-
+    Resolution [] resolutions;
+    public TMP_Dropdown resolutionDropdown;
     void Start()
     {
-        AudioMananger.instance.PlayAmbient(ambient);
+
+        resolutionDropdown.ClearOptions();
+        resolutions = Screen.resolutions;
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for ( int i = 0; i < resolutions.Length; i++ ) {
+
+            string option = resolutions [ i ].width + "x" + resolutions [ i ].height;
+
+            if ( !options.Contains(option) ) {
+            options.Add(option);                  
+
+            }
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions [ i ].height == Screen.currentResolution.height ) {
+                currentResolutionIndex = i;
+            } 
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     } 
 
     public void Menu()
@@ -24,6 +50,21 @@ public class Buttons : MonoBehaviour {
     public void Exit() {
         print("QUIT!");
         Application.Quit();
+    }
+    public void SetVolume(float volume ) {
+        print(volume);
+        //manejar el volumen del master desde aca
+    }
+    public void SetQuality(int index ) {
+        QualitySettings.SetQualityLevel(index);
+    }
+    public void SetFullScreen(bool toggle) {
+        Screen.fullScreen = toggle;
+        print("fullscreen");
+    }
+    public void SetResolucion(int index ) {
+        Resolution resolution = resolutions [ index ];
+        Screen.SetResolution(resolution.width, resolution.height,true);
     }
 
     //Borrar esto luegoh
